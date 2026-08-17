882876341a018ca7968da98d7cfcd1bb69c29eb0
DAIANE DOS SANTOS GONDIM <daianegondim@unipam.edu.br>
2026-08-17 20:42:38 -0300

    Add student registration system structure and main

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Aluno {
+    char nome[50];
+    int idade;
+    float nota;
+};
+
+int main() {
+    printf("Sistema de Cadastro de Alunos\n");
+    return 0;
+}
